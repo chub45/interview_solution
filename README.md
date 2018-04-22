29e55c7203aef5ad723723fef3b0b347bd101e52
Benson Chu <bchu@umail.ucsb.edu>
2018-04-22 15:48:08 -0700

    Finalized

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# interview_solution
+#Though Process:
+
+1. Unsure if expected to generate a 1:1 mapping (criteria did not specify) ie. multiple inputs can have same output
+2. If it was the case that a 1:1 mapping was expected, then uncommenting the code results in solving the 1:1 issue,
+   except in the case of having one less output. (There is a case where the key and element cannot be used because of
+   the circular relationship problem)
--- naiveSolution.cpp
+++ naiveSolution.cpp
@@ -2,39 +2,29 @@
 #include <vector>
 #include <map>
 #include <set>
+
 using namespace std;
 
 //function that creates and returns a map fulfilling the required criteria
 map<string, string> assignmentGenerator(vector<string> names, map<string, string> &previous){
     map<string, string> currAssignment;
-    set<string> hTable;
+    //set<string> hTable;
 	for(int i = 0;i < names.size(); i++){
         for(int j = 0; j < names.size(); j++){
-            //cout << "Checking: " << names[i] << ", " << names[j] << endl;
-            /*if(currAssignment.find(names[i]) == currAssignment.end()){         //check if key is in map already
-                cout << "This passes for " << names[i] << ", " << names[j] << endl;
-				if((names[i] != names[j]) &&                 //check to ensure name doesn't have self as pair 
-                   (currAssignment[names[j]] != names[i]) &&  //check if current assignment has converse
-				   (previous[names[i]] != names[j]) &&       //check if prev has pair
-                   (hTable.find(names[j]) == hTable.end())){ //check if second value used already
-
-                    cout << names[i] << "," << names[j] << " inserted" << endl;
-					currAssignment.insert(pair<string, string>(names[i], names[j]));
-					hTable.insert(names[j]);
-				}*/
 			if(currAssignment.find(names[i]) == currAssignment.end()){
-				if((names[i] != names[j]) && //check if pair involves same word twice
-				   (previous[names[i]] != names[j])){ //check if previous assignment has pair
-					if(currAssignment.find(names[j]) == currAssignment.end()){ //if names[j] doesn't exist yet
-						if(hTable.find(names[j]) == hTable.end()){
+				if((names[i] != names[j]) &&                                    //check if pair involves same word twice
+				   (previous[names[i]] != names[j])){                           //check if previous assignment has pair
+					if(currAssignment.find(names[j]) == currAssignment.end()){  //if names[j] doesn't exist yet
+						/*if(hTable.find(names[j]) == hTable.end()){
 							currAssignment.insert(pair<string, string>(names[i], names[j]));
 					        hTable.insert(names[j]);	
-						}
+						}*/
+                        currAssignment.insert(pair<string, string>(names[i], names[j]));
 					}
-				   	else if(currAssignment[names[j]] != names[i] && 
-                            hTable.find(names[j]) == hTable.end()){
+				   	else if(currAssignment[names[j]] != names[i] /*&& 
+                            hTable.find(names[j]) == hTable.end()*/){
 							currAssignment.insert(pair<string, string>(names[i], names[j]));
-						    hTable.insert(names[j]);
+						    //hTable.insert(names[j]);
 				    }
 				}
             }
